04b024ee1d74cf9c0b438129c01242495e27c874
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 <michellykaren15@gmail.com>
2024-03-14 03:06:04 -0300

    solução produtor/consumidor

--- Q3/q3.cpp
+++ Q3/q3.cpp
@@ -3,27 +3,67 @@
 #include <string>
 #include <queue>
 #include <mutex>
-#include <condition_variable>
+#include <condition_variable> // pra poder esperar até ter algo na fila pra consumir
 #include <thread>
+#include <regex>
 
-using namespace std;
-
-mutex mtex;
-condition_variable cv;
-queue<string> queue;
-bool done = false;
+std::mutex mtex;            // o trinco pra controlar acesso à fila
+std::condition_variable cv; // a condição pra esperar/sinalizar entre produtor/consumidor
+std::queue<std::string> q;  // a fila de mensagens
+bool done = false;          // pra saber quando terminou de produzir
 
 void produtor() {
-    ifstream input("Q3/input.xml");
-    string line;
-    string payload;
+    std::cout << "Produtor iniciado.\n";
+    std::ifstream input("input.xml");
+    std::string line;
+    std::string payload;
+    std::regex payloadRegex("<payload>(.*?)</payload>");
+
+    while (std::getline(input, line)) {
+
+        std::smatch matches;                            // aqui guarda o resultado da busca pela regex
+
+        if (std::regex_search(line, matches, payloadRegex) && matches.size() > 1) {
+            payload = matches[1].str();                 // pega só a parte que interessa
+
+            std::unique_lock<std::mutex> lock(mtex);    // trancou o trinco
+            q.push(payload);                            // põe na fila
+            std::cout << "Produtor: Mensagem " << payload << " adicionada.\n";
+            cv.notify_one();                            // avisa que tem coisa nova
+        }
+    }
+
+    std::unique_lock<std::mutex> lock(mtex);            // sinaliza que acabou
+    done = true;
+    cv.notify_all();
+    std::cout << "Produtor concluído.\n";
 }
 
-void consumidor() {}
+void consumidor() {
+    std::cout << "Consumidor iniciado.\n";              // vai ficar esperando ter algo na fila pra consumir
+    std::unique_lock<std::mutex> lock(mtex);
+
+    while (!done || !q.empty()) {                       // enquanto não terminou ou ainda tem coisa na fila
+        cv.wait(lock, []{return done || !q.empty();});  // espera ter algo
+        while (!q.empty()) {                            // enquanto tem coisa na fila
+            std::string payload = q.front();            // pega a primeira
+            q.pop();                                    // tira ela da fila
+            lock.unlock();                              // libera o trinco pra outra thread poder usar
+
+            std::cout << "Consumidor: " << payload << '\n';
+
+            lock.lock();                                // trancou de novo pra próxima volta do loop
+        }
+    }
+    std::cout << "Consumidor concluído.\n";
+}
 
 int main() {
-    thread t1(produtor);
-    thread t2(consumidor);
+    std::thread t1(produtor);
+    std::thread t2(consumidor);
+
+    t1.join();
+    t2.join();
 
     return 0;
 }
